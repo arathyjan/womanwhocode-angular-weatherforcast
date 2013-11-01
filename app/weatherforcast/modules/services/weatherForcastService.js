d32b6199ca7924b9596eb80eb763724821fe7063
arathyjan <arathyja@thoughtworks.com>
2013-11-01 16:51:02 +0530

    removing commented out code.. proper functional empty project to start with

--- app/weatherforcast/modules/services/weatherForcastService.js
+++ app/weatherforcast/modules/services/weatherForcastService.js
@@ -2,25 +2,5 @@
 
 angular.module('wwc.weatherForcast.services')
     .factory('WeatherService', ['$http', function ($http) {
-//
-//        var bedsForWard = function (uuid) {
-//            return $http.get("/openmrs/ws/rest/v1/admissionLocation/" + uuid, {
-//                method: "GET",
-//                params: {v: "full"},
-//                withCredentials: true
-//            });
-//        }
-//
-//        var assignBed = function (bedId, patientUuid) {
-//            var patientJson = {"patientUuid":patientUuid};
-//            return $http.post("/openmrs/ws/rest/v1/beds/"+ bedId, patientJson, {
-//                withCredentials: true,
-//                headers: {"Accept": "application/json", "Content-Type": "application/json"}
-//            });
-//        }
-//
-//        return {
-//            bedsForWard: bedsForWard,
-//            assignBed: assignBed
-//        };
+
     }]);
