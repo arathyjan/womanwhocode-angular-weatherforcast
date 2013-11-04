1cb0f63d6585c5645adbaf2d66ddf2ea7e856eb5
arathyjan <arathyja@thoughtworks.com>
2013-11-04 22:11:23 +0530

    Finished the view part , only CSS left

--- app/weatherforcast/modules/controllers/weatherForcastController.js
+++ app/weatherforcast/modules/controllers/weatherForcastController.js
@@ -4,12 +4,17 @@ angular.module('wwc.weatherForcast.controllers')
     .controller('WeatherController', ['$scope', '$location', 'WeatherService',
     function ($scope, $location, weatherService) {
 
+        $scope.condition = null;
         var woeidForBangalore = 2295420;
         var unit = 'c';
 
         $scope.getWeather = function () {
             weatherService.getWeatherForLocation(woeidForBangalore, unit).success(function(result){
-                console.log(result);
+                $scope.title = result.query.results.channel.item.title;
+                $scope.forecasts = result.query.results.channel.item.forecast;
+                $scope.condition = result.query.results.channel.item.condition;
+                $scope.image = "http://l.yimg.com/a/i/us/we/52/" + $scope.condition.code + ".gif";
             });
         }
+
     }]);
