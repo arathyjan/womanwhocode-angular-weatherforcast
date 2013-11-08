9baafabd695d5fb0c5025bfda4c3ed32fd6a8a54
Mewanbanjop <mewanm@thoughtworks.com>
2013-11-08 12:29:50 +0530

    Final step 1 <Mewan>

--- app/weatherforcast/modules/controllers/weatherForcastController.js
+++ app/weatherforcast/modules/controllers/weatherForcastController.js
@@ -1,15 +1,9 @@
-'use strict';
-
-angular.module('weatherForcast')
-    .controller('WeatherController', ['$scope',
-    function ($scope) {
-
-        $scope.title = "The Weather App";
+function WeatherController($scope){
+    $scope.title = "The Weather App";
         $scope.date = "Today";
         $scope.condition = {temp : "15 C", text : "Its chilly"};
         $scope.show = false;
-
         $scope.getWeather = function(){
-        	$scope.show = true;
+            $scope.show = true;
         }
-  }]);
+}
