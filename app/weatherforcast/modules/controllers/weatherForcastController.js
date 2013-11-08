2dd5a16fe4e4f0447b986b096329ca19c4162a1f
singhania <gauravs@thoughtworks.com>
2013-11-08 22:54:25 +0530

    Singhania | Some clean up and finishing touch

--- app/weatherforcast/modules/controllers/weatherForcastController.js
+++ app/weatherforcast/modules/controllers/weatherForcastController.js
@@ -4,17 +4,23 @@ angular.module('weatherForcast')
 .controller('WeatherController',['$scope','weatherForecastService',
     function ($scope,forecastService){
 
-    $scope.title = "The Weather App";
+    
         $scope.date = "Today";
         $scope.condition = {temp : "15 C", text : "Its chilly"};
         $scope.show = false;
         $scope.getWeather = function(){
-        	var data = forecastService.getWeatherForcastFor($scope.city,$scope.country,"c").then(function(data){
-        		$scope.condition={temp:data.query.results.channel.item.condition.temp};
-        		$scope.show = true;
+        	var data = forecastService.getWeatherForcastFor($scope.city,$scope.country,"c")
+        		.then(function(data){
+        			$scope.condition=data.query.results.channel.item.condition||{};
+                    $scope.forecasts = data.query.results.channel.item.forecast||[];
+                    $scope.condition.image="http://l.yimg.com/a/i/us/we/52/" + $scope.condition.code + ".gif";
+                    $scope.title=data.query.results.channel.item.title;
+        			$scope.show = true;
         	});
-        	
+        };
+        $scope.back=function(){
+            $scope.show=false;
 
-            
-        }}
+        }
+    }
 ]);
