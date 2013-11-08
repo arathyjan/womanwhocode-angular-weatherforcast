fcb2fa41b59ca5aeff550831452bd48664d2e691
Mewanbanjop <mewanm@thoughtworks.com>
2013-11-08 18:30:45 +0530

    Removing Routes and partial views<Mewan>

--- app/weatherforcast/app.js
+++ app/weatherforcast/app.js
@@ -1,7 +1,2 @@
 'use strict';
-
 var app = angular.module('weatherForcast',[]);
-app.config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/weatherforcast', {templateUrl: 'modules/views/weatherForcast.html', controller: 'WeatherController'});
-        $routeProvider.otherwise({redirectTo: '/weatherforcast'});
-}]);
--- app/weatherforcast/modules/controllers/weatherForcastController.js
+++ app/weatherforcast/modules/controllers/weatherForcastController.js
@@ -1,9 +1,13 @@
-function WeatherController($scope){
+'use strict';
+
+angular.module('weatherForcast')
+.controller('WeatherController',['$scope',
+    function ($scope){
     $scope.title = "The Weather App";
         $scope.date = "Today";
         $scope.condition = {temp : "15 C", text : "Its chilly"};
         $scope.show = false;
         $scope.getWeather = function(){
             $scope.show = true;
-        }
-}
+        }}
+]);
