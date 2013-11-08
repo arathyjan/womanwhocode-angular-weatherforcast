f02d2186836ee1c38e71f8b0630473f48b57def4
singhania <gauravs@thoughtworks.com>
2013-11-08 22:12:45 +0530

    singhania | handling error scenario

--- app/weatherforcast/modules/services/weatherForecastService.js
+++ app/weatherforcast/modules/services/weatherForecastService.js
@@ -2,23 +2,27 @@
 
 angular.module('weatherForcast').
 factory('weatherForecastService',['$http','$q',function($http,$q){
+	var fail = function(defer,msg){
+		alert(msg);
+		defer.reject();
+	}
 	var getWeatherForcastFor=function(city,country,unit){
 	var defer=$q.defer();
 	$http({
 		method:'JSONP',
 		url:"http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20geo.places%20where%20text%3D'"+city+","+country+"'&format=json&callback=JSON_CALLBACK"})
 	.success(function(data,status,headers,config){
-		console.log(data);
+		if(!data.query||!data.query.results||!data.query.results.place) fail(defer,"Please check the city and country and try again!!!");
 		$http({
            method: 'JSONP',
            url: "http://query.yahooapis.com/v1/public/yql?q=select item from weather.forecast " +
                    "where woeid='"+data.query.results.place.woeid+ "' and u='" + unit + "'&format=json&callback=JSON_CALLBACK"
             }).success(function(data){
-            			console.log(JSON.stringify(data));
+            	if(data.query.results.channel.item.title=="City not found") fail(defer,"Multiple cities possible");
             	defer.resolve(data)
-            });
+            }).error(function(){fail(defer,"Please check the city and country and try again!!!")});
 		})
-	.error(function(data,status,headers,config){alert("error");console.log(data)});
+	.error(function(){fail(defer,"Please check the city and country and try again!!!")});
 	return defer.promise;
 	};
 	return {
