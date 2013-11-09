9c5858e4adf910aa669e541ee8e7e26e89eccbac
singhania <gauravs@thoughtworks.com>
2013-11-09 09:52:21 +0530

    Revert "singhania | Added service with bare minimum functionality"
    This reverts commit 363de65bc23dc1a4f4c2a8808ac89b89eb9607f8.

--- app/weatherforcast/modules/controllers/weatherForcastController.js
+++ app/weatherforcast/modules/controllers/weatherForcastController.js
@@ -1,20 +1,13 @@
 'use strict';
 
 angular.module('weatherForcast')
-.controller('WeatherController',['$scope','weatherForecastService',
-    function ($scope,forecastService){
-
+.controller('WeatherController',['$scope',
+    function ($scope){
     $scope.title = "The Weather App";
         $scope.date = "Today";
         $scope.condition = {temp : "15 C", text : "Its chilly"};
         $scope.show = false;
         $scope.getWeather = function(){
-        	var data = forecastService.getWeatherForcastFor($scope.city,$scope.country,"c").then(function(data){
-        		$scope.condition={temp:data.query.results.channel.item.condition.temp};
-        		$scope.show = true;
-        	});
-        	
-
-            
+            $scope.show = true;
         }}
 ]);
