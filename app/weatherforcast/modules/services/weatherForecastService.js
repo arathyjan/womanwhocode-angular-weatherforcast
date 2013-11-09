d96406bd0126c68886c058b0bda6236006432f8d
arathyjan <arathyja@thoughtworks.com>
2013-11-09 09:35:11 +0530

    Some changes to be in par with step-2

--- app/weatherforcast/modules/services/weatherForecastService.js
+++ app/weatherforcast/modules/services/weatherForecastService.js
@@ -16,9 +16,16 @@ factory('weatherForecastService',['$http','$q',function($http,$q){
 			function(data,status,headers,config){
 				if(!data.query||!data.query.results||!data.query.results.place) 
 					fail(defer,"Please check the city and country and try again!!!");
+				var woeid;
+				if(data.query.results.place.length > 0){
+					woeid = data.query.results.place[0].woeid;
+				}
+				else{
+					woeid = data.query.results.place.woeid;
+				}
 				$http({
            			method: 'JSONP',
-           			url: "http://query.yahooapis.com/v1/public/yql?q=select item from weather.forecast where woeid='"+data.query.results.place.woeid+ "' and u='" + unit + "'&format=json&callback=JSON_CALLBACK"
+           			url: "http://query.yahooapis.com/v1/public/yql?q=select item from weather.forecast where woeid='"+woeid+ "' and u='" + unit + "'&format=json&callback=JSON_CALLBACK"
             	}).success(function(data){
         			if(data.query.results.channel.item.title=="City not found") 
         				fail(defer,"Multiple cities possible");
