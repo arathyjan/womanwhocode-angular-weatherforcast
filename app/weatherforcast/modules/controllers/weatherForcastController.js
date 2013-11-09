76490117be8c9bc8fdd748fce3deb6bc82a7f41a
singhania <gauravs@thoughtworks.com>
2013-11-09 09:49:25 +0530

    Revert "directives"
    This reverts commit f78943a12f633e6953e4a310eaedb54d995baa76.
    
    Conflicts:
    	app/weatherforcast/index.html

--- app/weatherforcast/modules/controllers/weatherForcastController.js
+++ app/weatherforcast/modules/controllers/weatherForcastController.js
@@ -3,6 +3,10 @@
 angular.module('weatherForcast')
 .controller('WeatherController',['$scope','weatherForecastService',
     function ($scope,forecastService){
+
+    
+        $scope.date = "Today";
+        $scope.condition = {temp : "15 C", text : "Its chilly"};
         $scope.show = false;
         $scope.getWeather = function(){
         	var data = forecastService.getWeatherForcastFor($scope.city,$scope.country,"c")
@@ -19,47 +23,4 @@ angular.module('weatherForcast')
 
         }
     }
-])
-   .directive('forecastdirective', function () {
-         return {
-            restrict: 'A',
-            scope: true,
-            template: '<div> {{ forecast.date }} ({{ forecast.day}}) - HIGH: {{ forecast.high}}&degC, Low: {{ forecast.low}}&degC'+
-                        '<div class="italic">' +
-                        '{{ forecast.text}}'+
-                        '</div>'+
-                        'Range : {{ forecast.range }} </br>'+
-                    ' </div>',
-            link: function(scope){
-                    scope.forecast.range = scope.forecast.high - scope.forecast.low;
-
-            }
-        }
-})
-    .directive('myAutocomplete', function ($parse) {
-        return function (scope, element, attrs) {
-            var ngModel = $parse(attrs.ngModel);
-            element.autocomplete({
-                autofocus: true,
-                minLength: 2,
-                source: function (request, response) {
-                    var results = ['Bangalore','Chennai','Pune','Gurgoan'];
-                        response(results);
-                },
-                select: function (event, ui) {
-                    scope.$apply(function (scope) {
-                        ngModel.assign(scope, ui.item.value);
-                        scope.$eval(attrs.ngChange);
-                    });
-                    return true;
-                },
-                search: function (event) {
-                    var searchTerm = $.trim(element.val());
-                    if (searchTerm.length < 2) {
-                        event.preventDefault();
-                    }
-                }
-            });
-        }
-    });
-
+]);
